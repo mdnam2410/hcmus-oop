2aa607fe7183f1b20c5f0609f38e2048fc619df1
Nam Mai <mdnam2410@gmail.com>
2020-12-02 23:09:57 +0700

    New pure virtual function, include library for std::string

--- Week08/8.2/Shape.h
+++ Week08/8.2/Shape.h
@@ -1,16 +1,19 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
+#include <string>
+
 class Shape {
 public:
     virtual ~Shape();
     
     virtual void info() const = 0;
 
+    virtual float perimeter() const = 0;
+
     virtual float area() const = 0;
 
     virtual std::string type() const = 0;
-
 };
 
 #endif
